a948ddafec28b5732cb18209fe2993148dcb5b37
Abhijit Nath <abhijitnath1a1@gmail.com>
2026-03-14 11:42:30 +0530

    added cors middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import { ENV } from './config/env';
 import { clerkMiddleware } from '@clerk/express';
 
 const app = express();
 
+app.use(
+  cors({
+    origin: ENV.FRONTEND_URL,
+  }),
+);
 app.use(clerkMiddleware()); // auth obj will be attached to the req obj
+app.use(express.json()); // parses JSON req bodies
+app.use(express.urlencoded({ extended: true })); // parses form data (like HTML Forms)
 
 app.get('/', (req, res) => {
   res.json({
