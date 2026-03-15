fe7c16f5ba4139678e705a58a6a238e35b880e6c
Abhijit Nath <abhijitnath1a1@gmail.com>
2026-03-15 16:31:34 +0530

    fixed the userSync hook & refactored useAuthReq hook

--- frontend/src/hooks/useUserSync.js
+++ frontend/src/hooks/useUserSync.js
@@ -16,7 +16,7 @@ export default function useUserSync() {
   useEffect(() => {
     if (isSignedIn && user && !isPending && !isSuccess) {
       syncUserMutation({
-        email: user.primaryEmailAddress.emailAddress,
+        email: user.primaryEmailAddress?.emailAddress,
         name: user.fullName || user.firstName,
         imageUrl: user.imageUrl,
       });
