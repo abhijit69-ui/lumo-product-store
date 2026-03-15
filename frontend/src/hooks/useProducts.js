799bfac1f76ba94d5a213a050f9ecd50ab95139e
Abhijit Nath <abhijitnath1a1@gmail.com>
2026-03-15 20:03:36 +0530

    Added invalidation queries for createProduct

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { createProduct, getAllProducts } from '../lib/api';
 
 export const useProducts = () => {
@@ -10,8 +10,12 @@ export const useProducts = () => {
 };
 
 export const useCreateProduct = () => {
+  const queryClient = useQueryClient();
   const result = useMutation({
     mutationFn: createProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['products'] });
+    },
   });
   return result;
 };
